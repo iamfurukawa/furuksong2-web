d51c63c837bc4d42f175edb5e947689b8cbfbdde
Vinicius Furukawa Carvalho <vinicius.furukawa.carvalho@gmail.com>
2026-01-20 21:23:39 -0300

    feat: changing login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -19,10 +19,10 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-card">
-        <h1>Soundboard</h1>
+        <h1>Furuksong2</h1>
         <form onSubmit={handleSubmit} className="login-form">
           <div className="form-group">
-            <label htmlFor="name">Seu nome</label>
+            <label htmlFor="name"></label>
             <input
               type="text"
               id="name"
